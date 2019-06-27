6554ddacd0d498fbd46904f8585be26ec360b32f
Unknown <guillaume.biausque@gmail.com>
2019-06-27 12:24:12 +0200

    Ajout services de Login
    Rajout de la redirection vers la pages landing
    Rajout et gestion du Logout
    Ajout du chemin pour le dashboard, de la page et du module correspondant
    Ajour d'une image pour la page Landing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+//import du Authguard
+import { AuthGuard } from './guard/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    // redirectTo: 'home',
+    // maintenant on redigire vers landing
+    redirectTo: 'landing',
     pathMatch: 'full'
   },
   {
@@ -18,7 +22,12 @@ const routes: Routes = [
   { path: 'landing', loadChildren: './pages/landing/landing.module#LandingPageModule' },
   { path: 'register', loadChildren: './pages/auth/register/register.module#RegisterPageModule' },
   { path: 'login', loadChildren: './pages/auth/login/login.module#LoginPageModule' },
-  { path: 'dashboard', loadChildren: './pages/dashboard/dashboard.module#DashboardPageModule' }
+  { path: 'dashboard', loadChildren: './pages/dashboard/dashboard.module#DashboardPageModule' },
+  // On ajoute les chemins pour : Dashboard/home & list
+  { path: 'dashboard', loadChildren: './pages/dashboard/dashboard.module#DashboardPageModule', canActivate: [AuthGuard] },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule', canActivate: [AuthGuard] },
+  //A voir si on garde list.
+  { path: 'list', loadChildren: './list/list.module#ListPageModule', canActivate: [AuthGuard] },
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
+// on ajoute le Navcontroller
+import { Platform, NavController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+// ajoute les services d'Authentification et d'Alert
+import { AuthService } from './services/auth.service';
+import { AlertService } from './services/alert.service';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +13,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent {
   public appPages = [
+    //on rajoute les info d'url
+    {
+      title: 'Dashboard',
+      url: '/dashboard',
+      icon: 'home'
+    },
     {
       title: 'Home',
       url: '/home',
@@ -25,7 +34,11 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    // AJoute au constructor
+    private authService: AuthService,
+    private navCtrl: NavController,
+    private alertService: AlertService
   ) {
     this.initializeApp();
   }
@@ -33,7 +46,23 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
+ // On commente le splashScreen.hide, il ne cachera donc pas "splashScreen" avant la vérification de l'authentification
+      //this.splashScreen.hide();
+      this.authService.getToken();
     });
+  }
+  // When Logout Button is pressed 
+  logout() {
+    this.authService.logout().subscribe(
+      data => {
+        this.alertService.presentToast(data['message']);        
+      },
+      error => {
+        console.log(error);
+      },
+      () => {
+        this.navCtrl.navigateRoot('/landing');
+      }
+    );
   }
 }
